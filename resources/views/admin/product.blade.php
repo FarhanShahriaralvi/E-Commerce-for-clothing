8cc10240c3cc3b0e868b4be762324e062c856a96
Farhan Shahriar Alvi <52394409+FarhanShahriaralvi@users.noreply.github.com>
2024-11-11 21:36:45 +0600

    Add files via upload
    all views are created

---
+++ resources/views/admin/product.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+
+    <style type="text/css">
+        .title{
+            color:white; 
+            padding-top: 25px; 
+            font-size:25px;
+        }
+
+        label{
+            display: inline-block;
+            width: 200px;
+        }
+    </style>
+  </head>
+  <body>
+        @include('admin.sidebar')
+      <!-- partial -->
+      
+
+      @include('admin.navbar')
+
+      <div class="container-fluid page-body-wrapper">
+        <div class="container" align="center">
+        <!-- partial -->
+        <h1 class="title">Add Product </h1>
+
+        
+
+        @if(session()->has('message'))
+
+        <div class="alert alert-success">
+          <button type="button" class="close" data-dismiss="alert">x</button>
+          {{session()->get('message')}}
+        </div>
+      @endif
+        
+        <form action="{{url('uploadproduct')}}" method="post" enctype="multipart/form-data">
+
+        @csrf
+
+        <div style="padding:15px;">
+        <label >Product Title</label>
+        <input style="color:black;" type="text" name="title" placeholder="Give a Product Title" require="">
+        </div>
+
+        <div style="padding:15px;">
+        <label>Price</label>
+        <input style="color:black;" type="number" name="price" placeholder="Give a Price" require="">
+        </div>
+
+        <div style="padding:15px;">
+        <label>Description</label>
+        <input style="color:black;" type="text" name="description" placeholder="Give a Description" require="">
+        </div>
+
+        <div style="padding:15px;">
+        <label>Quantity</label>
+        <input style="color:black;" type="text" name="quantity" placeholder="Product Quantity" require="">
+        </div>
+
+        <div style="padding:15px;">
+        <input type="file" name="file">
+        </div>
+
+        <div style="padding:15px;">
+        <input class="btn btn-success" type="submit">
+        </div>
+
+
+        </form>
+
+
+
+
+
+
+        </div>
+
+        
+</div>
+          <!-- partial -->
+        @include('admin.script')
+  </body>
+</html>
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,64 @@
+<div class="container-scroller">
+      <div class="row p-0 m-0 proBanner" id="proBanner">
+        <div class="col-md-12 p-0 m-0">
+          <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+            
+            <div class="d-flex align-items-center justify-content-between">
+              <a href="https://www.bootstrapdash.com/product/corona-free/"><i class="mdi mdi-home me-3 text-white"></i></a>
+              <button id="bannerClose" class="btn border-0 p-0">
+                <i class="mdi mdi-close text-white me-0"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+          <a class="sidebar-brand brand-logo" href="index.html"><img src="assets/images/logo.svg" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+        </div>
+        <ul class="nav">
+          
+          <li class="nav-item nav-category">
+            <span class="nav-link">Navigation</span>
+          </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="index.html">
+              <span class="menu-icon">
+                <i class="mdi mdi-speedometer"></i>
+              </span>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('product')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-file-document-box"></i>
+              </span>
+              <span class="menu-title">Add Products</span>
+            </a>
+          </li>
+
+
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('showproduct')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-file-document-box"></i>
+              </span>
+              <span class="menu-title">Show Products</span>
+            </a>
+          </li>
+
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('showorder')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-file-document-box"></i>
+              </span>
+              <span class="menu-title">Orders</span>
+            </a>
+          </li>
+
+        </ul>
+      </nav>
